ed1a5d9e2b497c6386d2fab6b601c0416ed7cd95
Josh <josh@gdovindesigns.com>
2020-09-28 12:28:35 -0400

    updated demo

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -1,17 +1,20 @@
 import {gsap} from "gsap";
 
-var redSpeed = 1;
+// var redSpeed = 1;
 var blueSpeed = 1;
 
+gsap.to(".red-box",{duration: blueSpeed, x: 300});
+gsap.to(".blue-box",{duration: blueSpeed, x: 300, delay:1});
+gsap.to(".orange-box",{duration: blueSpeed, x: 300, delay:2});
 
 // TweenMax.to(".red-box",redSpeed, {x: 300, borderRadius: "50px", alpha: 0});
 // TweenLite.to(".red-box",redSpeed,{x:800, borderRadius: "200px", delay: redSpeed});
 
 
 // what do you want to animate? / How long should animation be? / What is it you want to animate?
-gsap.to(".red-box",{duration: redSpeed, x: 300, borderRadius: "50px", alpha: 0});
-gsap.to(".red-box",{duration: redSpeed, x:800, borderRadius: "200px", delay: redSpeed});
+// gsap.to(".red-box",{duration: redSpeed, x: 300, borderRadius: "50px", alpha: 0});
+// gsap.to(".red-box",{duration: redSpeed, x:800, borderRadius: "200px", delay: redSpeed});
 
 
-gsap.from(".blue-box",{duration: blueSpeed, x: 300, borderRadius: "100px"});
-gsap.to(".blue-box",{duration: blueSpeed, x: 800, borderRadius: "100px", delay:blueSpeed });
+// gsap.from(".blue-box",{duration: blueSpeed, x: 300, borderRadius: "100px"});
+//gsap.to(".blue-box",{duration: blueSpeed, x: 800, borderRadius: "100px", delay:blueSpeed });
