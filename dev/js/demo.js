0ddb0c149fed184cd92311895b211abfb5ec48ab
Josh <josh@gdovindesigns.com>
2020-10-14 14:35:35 -0400

    icon demo completed

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -1,13 +1,65 @@
 import {gsap} from "gsap";
 
-gsap.set("#ice-cream-cp",{transformOrigin:"center"});
+gsap.set("#overlay",{transformOrigin:"center"});
+gsap.set("#progress",{transformOrigin:"center"});
 
-const iceCreamTL = gsap.timeline();
-iceCreamTL.from("#clip-demo",{duration:3, x:-400, ease:"none", scale:3},"startIceCream")
-            .from("#ice-cream",{duration:1, y:-400, ease:"none"})
-            .to("#ice-cream",{duration:3, y:400, ease:"none"},"startIceCream");
+var RAD  = Math.PI / 180;
+var PI_2 = Math.PI / 2;
+
+var clipPath = document.querySelector("#arcPath");
+
+var arc = {  
+  start: 360,
+  end: 0,
+  cx: 150,
+  cy: 150,
+  r: 80 
+};
+
+const iconTL = gsap.timeline();
+
+iconTL.from("#overlay",{duration:0.5, alpha:0, ease:"none", scale:3})
+    .from("#progress",{duration:0.5, alpha:0, ease:"none", scale:0},"-=0.25")
+    .to(arc, 3, { end: 360, ease: "none", onUpdate: updatePath})
+    .to("#overlay",{duration:0.5, alpha:0, ease:"none", scale:3});
 
 
-export function iceCreamAnimation(){
-    return iceCreamTL;
+export function iconAnimation(){
+    return iconTL;
 }
+
+
+
+
+
+// gsap.to(arc, 3, { end: 360, ease: "none", onUpdate: updatePath});
+
+updatePath();
+
+function updatePath() {
+  clipPath.setAttribute("d", getPath(arc.cx, arc.cy, arc.r, arc.end, arc.start)); 
+}
+
+function getPath(cx, cy, r, a1, a2) {
+   
+  var delta = a2 - a1;
+  
+  if (delta === 360) {
+        
+    return "M " + (cx - r) + " " + cy + " a " + r + " " + r + " 0 1 0 " + r * 2 + " 0 a " + r + " " + r + " 0 1 0 " + -r * 2 + " 0z"; 
+  }
+  
+  var largeArc = delta > 180 ? 1 : 0;
+    
+  a1 = a1 * RAD - PI_2;
+  a2 = a2 * RAD - PI_2;
+
+  var x1 = cx + r * Math.cos(a2);   
+  var y1 = cy + r * Math.sin(a2);
+
+  var x2 = cx + r * Math.cos(a1); 
+  var y2 = cy + r * Math.sin(a1);
+    
+  return "M " + x1 + " " + y1 + " A " + r + " " + r + " 0 " + largeArc + " 0 " + x2 + " " + y2 + " L " + cx + " " + cy + "z";
+}
+
--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,5 +1,5 @@
 import gsap from "gsap/gsap-core"
-import {iceCreamAnimation} from "./demo.js"
+import {iconAnimation} from "./demo.js"
 
 const mainTL = gsap.timeline();
-mainTL.add(iceCreamAnimation());
+mainTL.add(iconAnimation());
