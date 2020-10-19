58ed1e2409361c01f308ae9b9fd13be3952d54fa
Josh <josh@gdovindesigns.com>
2020-10-19 14:16:48 -0400

    toggle push

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -1,4 +1,10 @@
 import {gsap} from "gsap";
 
+const toggleTL = gsap.timeline();
 
-console.log(gsap);
+toggleTL.to("#background",{duration: 0.5, fill:"#f61bfe"},"toggle")
+        .to("#toggle",{duration: 0.5, fill:"#fff"},"toggle");
+
+export function toggleAnimation(){
+    return toggleTL;
+}
--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,4 +1,25 @@
 import * as Demo from './demo.js';
 
-// this will keep CodeKit quite!
-console.log(Demo);
+import $ from "jquery";
+import { gsap } from "gsap";
+import { toggleAnimation } from './demo.js';
+
+var toggle = false;
+
+const mainTl = gsap.timeline({paused:true});
+mainTl.add(toggleAnimation());
+
+$("#toggle-button").on("click", function(){
+    console.log("working");
+    if(toggle === false){
+
+        mainTl.play();
+        toggle = true;
+        // gsap.set("#toggle-button",{alpha:0});
+
+    }else{
+        mainTl.reverse();
+        toggle = false;
+        // gsap.set("#toggle-button",{alpha:1});
+    }
+})
