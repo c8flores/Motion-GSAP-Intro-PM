7ca14e38546a84543c207d19863d8f94b5756067
Josh <josh@gdovindesigns.com>
2020-10-21 13:48:55 -0400

    background animation complete

---
+++ dev/js/background/background-min.js
@@ -0,0 +1,1 @@
+import{gsap}from"gsap";const backgroundTL=gsap.timeline();backgroundTL.from("#moon",{duration:3,y:1e3});export function backgroundAnimation(){return backgroundTL}
--- dev/js/background/background.js
+++ dev/js/background/background.js
@@ -2,9 +2,12 @@ import {gsap} from "gsap"
 
 const backgroundTL = gsap.timeline();
 
+gsap.set(".rings",{transformOrigin: "center"});
+
 
 export function backgroundAnimation(){
-    backgroundTL.from("#moon",{duration:3, y:1000});
+    backgroundTL.from("#moon-rings",{duration:3, y:1000})
+    .from(".rings",{duration:1, scale:0, stagger:-0.5, ease: "back.out(1.7)"},"-=2");
 
     return backgroundTL;
 
